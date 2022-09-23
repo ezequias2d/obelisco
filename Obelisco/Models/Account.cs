1f6b5a4549df76a5f2f43f35e791b1b19a4dd434
ezequias2d <ezequiasmoises@gmail.com>
2022-09-23 13:39:48 -0300

    Change ECCurve

--- Obelisco/Models/Account.cs
+++ Obelisco/Models/Account.cs
@@ -5,46 +5,46 @@ namespace Obelisco;
 
 public class Account
 {
-    public Account()
-    {
-        Key = ECDsa.Create(ECCurve.CreateFromFriendlyName("Curve25519"));
-    }
+	public Account()
+	{
+		Key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+	}
 
-    public Account(ReadOnlySpan<byte> publicKey)
-    {
-        Key = ECDsa.Create(ECCurve.CreateFromFriendlyName("Curve25519"));
-        Key.ImportSubjectPublicKeyInfo(publicKey, out _);
-    }
+	public Account(ReadOnlySpan<byte> publicKey)
+	{
+		Key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+		Key.ImportSubjectPublicKeyInfo(publicKey, out _);
+	}
 
-    public Account(ReadOnlySpan<byte> privateKey, ReadOnlySpan<byte> password)
-    {
-        Key = ECDsa.Create(ECCurve.CreateFromFriendlyName("Curve25519"));
-        Key.ImportEncryptedPkcs8PrivateKey(password, privateKey, out _);
-    }
+	public Account(ReadOnlySpan<byte> privateKey, ReadOnlySpan<byte> password)
+	{
+		Key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+		Key.ImportEncryptedPkcs8PrivateKey(password, privateKey, out _);
+	}
 
-    private ECDsa Key { get; }
+	private ECDsa Key { get; }
 
-    public byte[] PublicKey => Key.ExportSubjectPublicKeyInfo();
+	public byte[] PublicKey => Key.ExportSubjectPublicKeyInfo();
 
-    public byte[] ExportEncryptedPkcs8PrivateKey(ReadOnlySpan<byte> password)
-    {
-        return Key.ExportEncryptedPkcs8PrivateKey(
-            password,
-            new PbeParameters(
-                PbeEncryptionAlgorithm.Aes256Cbc,
-                HashAlgorithmName.SHA512,
-                32
-            )
-        );
-    }
+	public byte[] ExportEncryptedPkcs8PrivateKey(ReadOnlySpan<byte> password)
+	{
+		return Key.ExportEncryptedPkcs8PrivateKey(
+			password,
+			new PbeParameters(
+				PbeEncryptionAlgorithm.Aes256Cbc,
+				HashAlgorithmName.SHA512,
+				32
+			)
+		);
+	}
 
-    public byte[] SignData(byte[] data)
-    {
-        return Key.SignData(data, 0, data.Length, HashAlgorithmName.SHA512, DSASignatureFormat.Rfc3279DerSequence);
-    }
+	public byte[] SignData(byte[] data)
+	{
+		return Key.SignData(data, 0, data.Length, HashAlgorithmName.SHA512, DSASignatureFormat.Rfc3279DerSequence);
+	}
 
-    public bool VerifyData(byte[] data, byte[] signature)
-    {
-        return Key.VerifyData(data, signature, HashAlgorithmName.SHA512, DSASignatureFormat.Rfc3279DerSequence);
-    }
+	public bool VerifyData(byte[] data, byte[] signature)
+	{
+		return Key.VerifyData(data, signature, HashAlgorithmName.SHA512, DSASignatureFormat.Rfc3279DerSequence);
+	}
 }
